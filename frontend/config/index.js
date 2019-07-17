3c81f329e313387f1b4a08755432ad882043fd4f
Spanri <kozlova9v@mail.ru>
2019-07-18 02:09:46 +0300

    fix bugs

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -41,7 +41,7 @@ module.exports = {
     index: path.resolve(__dirname, '../../backend/dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../../backend/'),
+    assetsRoot: path.resolve(__dirname, '../../backend'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
